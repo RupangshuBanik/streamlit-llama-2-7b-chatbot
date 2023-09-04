d6cf1876b0db4d51220714d015113fbca0f7a66b
Rupangshu Banik <rupbanik00@gmail.com>
2023-09-04 20:56:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 @st.cache_resource()
 def ChatModel(temperature, top_p):
     return AutoModelForCausalLM.from_pretrained(
-        'TheBloke/Llama-2-7B-Chat-GGML', 
+        'llama-2-7b-chat.ggmlv3.q5_0.bin', 
         model_type='llama',
         temperature=temperature, 
         top_p = top_p)
